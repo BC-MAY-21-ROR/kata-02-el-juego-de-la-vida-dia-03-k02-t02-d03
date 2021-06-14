8598b495f21517bf51c303f844c13834164deb99
Jairo Olan <olan.mayo05@gmail.com>
2021-06-14 18:35:24 -0500

    Drawing cells,
    Co-authored-by: Angel Oramas <oramasrojas199angelarturo@hotmail.com>
    Co-authored-by: Ricardo Jimenez <joserjc.96@gmail.com>

--- src/game.rb
+++ src/game.rb
@@ -1,7 +1,51 @@
-# frozen_string_literal: true
+require './board.rb'
 
-require './grid'
+# Controls game play
+class Game < Board
+  attr_accessor :board
 
-class Game
-    
+  def draw_figure(figure)
+    figure.each do |position|
+      cell = @board[position[0]][position[1]]
+      revive(cell)
+      notify_status(cell)
+    end
+  end
+
+  def next_generation
+    cells_changed = []
+    @board.each do |row|
+      row.each do |cell|
+        old_status = cell.alive?
+        check_rules(cell)
+
+        cells_changed.push(cell) unless old_status == cell.alive?
+      end
+    end
+
+    cells_changed.each { |cell| notify_status(cell) }
+  end
+
+  def notify_status(cell)
+    cell.neighbors.each do |position|
+      neighbor = @board[position[:row]][position[:column]]
+      cell.alive? ? neighbor.nearby += 1 : neighbor.nearby -= 1
+    end
+  end
+
+  def kill(cell)
+    cell.status = false if cell.alive?
+  end
+
+  def revive(cell)
+    cell.status = true unless cell.alive?
+  end
+
+  def check_rules(cell)
+    if cell.alive?
+      kill(cell) if cell.nearby <= 1 || cell.nearby >= 4
+    elsif cell.nearby == 3
+      revive(cell)
+    end
+  end
 end
